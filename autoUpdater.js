1cb36d53356cb90193a833df56ffa8df64a05d21
ShadowBeast <ShadovvBeast@gmail.com>
2019-08-16 14:02:10 +0300

    change updater to use git

--- autoUpdater.js
+++ autoUpdater.js
@@ -9,49 +9,33 @@ module.exports = async (url) => {
     const fetchRemotePackage = await axios.get('https://raw.githubusercontent.com/dotai2012/ultimate-bot/master/package.json');
     const remoteVersion = fetchRemotePackage.data.version;
     const remoteDependencies = fetchRemotePackage.data.dependencies;
-
+    const execCallback = (e, stdout, stderr) => {
+      if (e instanceof Error) {
+        console.error(e);
+        throw e;
+      }
+      console.log(stdout);
+      console.error(stderr);
+    };
     if (version !== remoteVersion) {
-      const { data } = await axios({
-        method: 'get',
-        url,
-        responseType: 'stream',
-      });
-      const file = fs.createWriteStream('master.zip');
-      data.pipe(file);
+      exec('git fetch upstream', execCallback);
+      exec('git checkout master', execCallback);
+      exec('git merge upstream/master', execCallback);
+      exec('git push', execCallback);
 
-      file.once('finish', async () => {
-        const zip = new AdmZip('master.zip');
-        zip.extractAllTo('./', true);
-
-        const extractedFolder = `./${name}-master`;
-        const listEntries = await fs.readdir(extractedFolder);
-        await Promise.all(listEntries.map(async entry => fs.move(`${extractedFolder}/${entry}`, `./${entry}`, { overwrite: true })));
-
-        await fs.remove('master.zip');
-        await fs.remove(extractedFolder);
-
-        const dependenciesKey = Object.keys(dependencies);
-        const remoteDependenciesKey = Object.keys(remoteDependencies);
-        if (dependenciesKey.length !== remoteDependenciesKey.length) {
-          exec('npm install', (e, stdout, stderr) => {
-            if (e instanceof Error) {
-              console.error(e);
-              throw e;
-            }
-            console.log(stdout);
-            console.error(stderr);
-          });
-        }
-
-        process.send({ isUpdate: true });
-        process.exit(0);
-        return false;
-      });
+      const dependenciesKey = Object.keys(dependencies);
+      const remoteDependenciesKey = Object.keys(remoteDependencies);
+      if (dependenciesKey.length !== remoteDependenciesKey.length) 
+          exec('npm install', execCallback);
+      process.send({ isUpdate: true });
+      process.exit(0);
+      return false;
     } else {
       console.log('The software is up to date');
       return true;
     }
-  } catch (e) {
-    return true;
+  }
+   catch (e) {
+      return true;
   }
 };
